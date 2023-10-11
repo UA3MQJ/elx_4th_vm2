f0a8d8edf65473d5489d36003c339699fb222c46
Alexey Bolshakov <ua3mqj@gmail.com>
2023-10-11 18:00:07 +0300

    1.1 Базовые свойства форт системы: rs, ds, ip, wp

--- lib/e4vm.ex
+++ lib/e4vm.ex
@@ -6,6 +6,13 @@ defmodule E4vm do
   require Logger
   alias Structure.Stack
 
+  defstruct [
+    rs: Structure.Stack.new(), # Стек возвратов
+    ds: Structure.Stack.new(), # Стек данных
+    ip: 0,                     # Указатель инструкций
+    wp: 0,                     # Указатель слова
+  ]
+
   def new() do
     %E4vm{}
   end
