c5c0e097934d5b76e8559061d09f3b5d0ee3b6b8
Alexey Bolshakov <ua3mqj@gmail.com>
2023-10-11 15:07:50 +0300

    1. Создаем базовый модуль E4vm.

---
+++ lib/e4vm.ex
@@ -0,0 +1,13 @@
+defmodule E4vm do
+  @moduledoc """
+  Documentation for `E4vm`.
+  ds word size - 16 bit
+  """
+  require Logger
+  alias Structure.Stack
+
+  def new() do
+    %E4vm{}
+  end
+
+end
